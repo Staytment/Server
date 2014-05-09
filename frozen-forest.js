c4b8462ae643c37f2f60d11c65231dcbf26369eb
Soeren Wegener <wegener92@gmail.com>
2014-05-09 17:52:21 +0200

    Upgraded to express 4.0
    Also did some minor upgrades of several other packages.

--- frozen-forest.js
+++ frozen-forest.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var compression = require('compression');
+var bodyParser = require('body-parser');
 var logfmt = require('logfmt');
 var validator = require('./data-validator');
 var swagger = require('swagger-node-express');
@@ -10,13 +12,13 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 app.use(logfmt.requestLogger());
-app.use(express.compress());
-app.use(express.json());
+app.use(compression());
+app.use(bodyParser.json());
 app.use (function (error, req, res, next){
   //Catch json error
   res.send(res, {code: 400, message: 'Bad request'});
 });
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded());
 app.use(validator());
 
 swagger.setAppHandler(app);
@@ -50,6 +52,7 @@ swagger.addDelete(posts.deletePost);
 
 
 swagger.configure("http://api.staytment.com", "0.1");
+//swagger.configure("http://localhost:5000", "0.1");
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function () {
   console.log('Listening on localhost:' + port);
