f630b074f5018b98932184a38385aa200d80f77c
Soeren Wegener <wegener92@gmail.com>
2014-07-03 00:18:40 +0200

    Bumped version.

--- frozen-forest.js
+++ frozen-forest.js
@@ -57,7 +57,7 @@ swagger.addGet(posts.getPost);
 swagger.addPost(posts.createPost);
 swagger.addDelete(posts.deletePost);
 
-swagger.configure(process.env.DOMAIN_API || 'http://localhost:5000', '0.1');
+swagger.configure(process.env.DOMAIN_API || 'http://localhost:5000', '0.1.5');
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function () {
   console.log('Listening on localhost:' + port);
